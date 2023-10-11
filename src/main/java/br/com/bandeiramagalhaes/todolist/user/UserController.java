3518aecf728d1f0d97ca5bd33a96933dca8b5bf1
Bandeira Magalhães <bandeiramagalhaesoficial@gmail.com>
2023-10-11 14:02:35 -0300

    criação do banco de dados em memória com o JPA e avanços no userController

--- src/main/java/br/com/bandeiramagalhaes/todolist/user/UserController.java
+++ src/main/java/br/com/bandeiramagalhaes/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package br.com.bandeiramagalhaes.todolist.user;
 
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+  @Autowired
+  private IUserRepository userRepository;
 
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.name);
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+    if (user != null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
+    }
+    return ResponseEntity.status(HttpStatus.CREATED).body(this.userRepository.save(userModel));
   }
 }
